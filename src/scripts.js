6c282be020c6d84c7d9695a128b979b75a8b8fc6
Deodat Kimuene <dkimuene1@gmail.com>
2022-02-16 21:12:36 -0600

    Load the Questions
    Added question id to h2 tag; Added id to each list element tag. Added the function loadQuiz and a submit button event listener

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,4 @@
 function init() {
-    
     const quizData = [
         {
             question: "Who was the first American woman in space?",
@@ -8,14 +7,16 @@ function init() {
             c: "Sally Ride",
             d: "Svetlana Savitskaya",
             correct: 'c'
-        },{
+        },
+        {
             question: "(5 + 3)/2 * 10 = ? ",
             a: "40",
             b: "45",
             c: "3/4",
             d: "20",
             correct: 'a'
-        },{
+        },
+        {
             
             question: "What is the minimum crew size for the ISS? ",
             a: "5",
@@ -23,15 +24,17 @@ function init() {
             c: "7",
             d: "12",
             correct: 'b'
-        },{
+        },
+        {
             question: "Who is in charge of the executive branch?",
             a: "The Speaker of the House",
             b: "The Prime Minister",
             c: "The President",
             d: "The Chief Justice",
             correct: "c"
-
-        },{
+    
+        },
+        {
             question: "We elect a U.S. Senator for how many years?",
             a: "10",
             b: "4",
@@ -39,74 +42,39 @@ function init() {
             d: "6",
             correct:"d"
         }
-    ]
+    ];
+    
+    const questionElement = document.getElementById('question')
+    const a_text = document.getElementById('a_text');
+    const b_text = document.getElementById("b_text");
+    const c_text = document.getElementById("c_text");
+    const d_text = document.getElementById("d_text");
+    const submitBtn = document.getElementById('submit');
+    
+    let currentQuestion = 0;
+    
+    loadQuiz();
+    
+    function loadQuiz(){
+        const currentQuizData = quizData[currentQuestion];
+      
+        questionElement.innerText = currentQuizData.question;
+    
+        a_text.innerText = currentQuizData.a;
+        b_text.innerText = currentQuizData.b;
+        c_text.innerText = currentQuizData.c;
+        d_text.innerText = currentQuizData.d;
+     
+    }
+    submitBtn.addEventListener("click", () => {
+        currentQuestion++;
+        if(currentQuestion < quizData.length){
+            loadQuiz();
+        } else {
+            alert('You are finish')
+        }
+    })
+    
 }
-
 window.addEventListener("load", init)
 
-// const input = require('readline-sync');
-
-// TODO 2: modify your quiz app to ask 5 questions //
-
-// TODO 1.1a: Define candidateName // 
-let candidateName = "";
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let quizData = [];
-let correctAnswers = [];
-let candidateAnswers = [];
-
-
-// function askForName() {
-//   // TODO 1.1b: Ask for candidate's name //
-//    return candidateName = input.question('What is your name?:  ');
-// }
-
-function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  console.log("Lets begin the Quiz.\n ")
-  for (let i = 0; i < questions.length; i++){
-    candidateAnswer = input.question(questions[i]);
-    candidateAnswers.push(candidateAnswer)
-  }
-
-}
-// function gradeQuiz(candidateAnswers) {
-
-//   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-//   let correctCandidateAnswers = 0
-//   console.log("\n" + 'Candidate Name: ' + candidateName + "\n");
-//   for (let i = 0; i < candidateAnswers.length; i++){
-//     count = i+1;
-//     console.log(`${count}) ${questions[i]}\n Your Answer: ${candidateAnswers[i]}\n Correct Answer: ${correctAnswers[i]}\n`);
-//     if (correctAnswers[i].toLowerCase() === candidateAnswers[i].toLowerCase()){
-//       correctCandidateAnswers += 1 ;
-//       }
-//     }
-  
-
-
-//   let grade;
-  
-
-//   return grade;
-// }
-
-// function runProgram() {
-//   askForName();
-//   // TODO 1.1c: Ask for candidate's name //
-//   console.log("Hello " + candidateName + ', nice to meet you.');
-  
-//   askQuestion();
-//   gradeQuiz(this.candidateAnswers);
-// }
-
-// Don't write any code below this line //
-// And don't change these or your program will not run as expected //
-// module.exports = {
-//   candidateName: candidateName,
-//   questions: questions,
-//   correctAnswers: correctAnswers,
-//   candidateAnswers: candidateAnswers,
-//   gradeQuiz: gradeQuiz,
-//   runProgram: runProgram
-// };
